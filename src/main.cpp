69369e1bcce7a23229f4edaf3dfc2a7db289d293
Yiran Wang <yiranmushroom@gmail.com>
2026-01-02 04:12:18 -0800

    fix sync problems.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@ import Core.Entrance;
 import Vendor.ApplicationAPI;
 import "SDL3/SDL.h";
 import Core.Application;
+import ImGui.ImGuiApplication;
 
 namespace
 Engine {
     int Main(int argc, char **argv) {
-        Application app;
+        ImGuiApplication app;
         app.Init({
             .Title = "Frosty Engine App"
         });

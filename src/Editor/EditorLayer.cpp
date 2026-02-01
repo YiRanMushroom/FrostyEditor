0a46f5fa95d39d6442aee1a06ccf4b33dc48e606
Yiran Wang <yiranmushroom@gmail.com>
2026-01-31 21:41:01 -0800

    Implemented MultiInterface.

--- src/Editor/EditorLayer.cpp
+++ src/Editor/EditorLayer.cpp
@@ -54,9 +54,8 @@ namespace Editor {
         Engine::Renderer2DDescriptor desc{};
         desc.OutputSize = {1920, 1080};
         mCamera = Engine::MakeRef<PerspectiveCamera>();
-        desc.Transforms = std::vector{
-            mCamera.As<Engine::ITransform>()
-        };
+        Engine::Borrowed<Engine::ITransform> iTransform[1] = {mCamera.Borrow().Into<Engine::ITransform>()};
+        desc.Transforms = std::span(iTransform);
         mRenderer = Engine::MakeRef<Engine::NVRenderer2D>(desc, mApp->GetCommandListSubmissionContext());
 
         InitializeFontAsync();
